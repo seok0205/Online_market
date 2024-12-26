d369cbab5a57efd31425c07b61881832fd11c34b
seok0205 <9526yu@naver.com>
2024-12-27 06:28:47 +0900

    update - membership func
    1. 회원가입
    2. 정보 수정 및 회원 탈퇴

--- online_market/accounts/views.py
+++ online_market/accounts/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import (
     AuthenticationForm,
+    PasswordChangeForm,
 )
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
 from django.views.decorators.http import (
     require_POST,
     require_http_methods,
 )
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 @require_http_methods(["GET", "POST"])
 def login(request):
@@ -27,3 +31,49 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect("home")
+
+@require_http_methods(["GET", "POST"])
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
+
+@require_POST
+def delete(request):
+    if request.user.is_authenticated:
+        request.user.delete()
+        auth_logout(request)
+    return redirect("home")
+
+@require_http_methods(["GET", "POST"])
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {"form": form}
+    return render(request, "accounts/update.html", context)
+
+@login_required
+@require_http_methods(["GET", "POST"])
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect("home")
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {"form": form}
+    return render(request, "accounts/change_password.html", context)
