062aaf38687cd9ee76fe9eba10e9f5fec3b8ce46
seok0205 <9526yu@naver.com>
2024-12-27 09:21:22 +0900

    update - Product model
    물품의 이름, 내용, 생성 날짜, 수정 날짜, 이미지, 등록자

--- online_market/products/models.py
+++ online_market/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(
+        upload_to='images/',
+        blank=True,
+        null=True,
+    )
+    
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+    
+    def __str__(self):
+        return self.name
